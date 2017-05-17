eebbc30931ce35f81074b4fe904272e77b4c0926
jaythemishra <jaythemishra@gmail.com>
2017-05-17 15:08:01 -0700

    Added basic background music.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 import java.awt.event.*;
 import javax.swing.*;
+
+import jay.jaysound.JayLayer;
+import jay.jaysound.JayLayerListener;
+
 import java.awt.*;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements JayLayerListener {
 
 	JPanel cardPanel;
+	
+	private JayLayer sound;
+	
+	private String[] songs;
+
 	
 	/**
 	 * Creates a new Main object that runs the program.
 	 * @param title The title of the window
 	 */
 	public Main(String title) {
+		
 		super(title);
+		String[] songs = new String[]{"game1.mp3"};
 		setBounds(100, 100, 800, 800);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    
@@ -31,6 +42,15 @@ public class Main extends JFrame {
 	    cardPanel.add(instructions,"instructions");
 	    cardPanel.add(settings,"settings");
 	    cardPanel.add(game,"game");
+	    
+	    sound=new JayLayer("audio/","audio/",false);
+		sound.addPlayList();
+		sound.addSongs(0,songs);
+		//sound.addSoundEffects(soundEffects);
+		sound.changePlayList(0);
+		sound.addJayLayerListener(this);
+		sound.nextSong();
+
 	    
 	    add(cardPanel);
 	
@@ -54,5 +74,31 @@ public class Main extends JFrame {
 		((CardLayout)cardPanel.getLayout()).show(cardPanel,name);
 		requestFocus();
 	}
+
+	@Override
+	public void musicStarted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void musicStopped() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playlistEnded() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void songEnded() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
   
 }
